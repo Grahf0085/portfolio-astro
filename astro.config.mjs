9d00d3d413ffc3d9c70d0f8bfb3598a8b3af34c3
Tucker Hoog <77054686+Grahf0085@users.noreply.github.com>
2024-10-28 19:15:27 -0400

    add output hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'hybrid',
   server: {port: 3000, host: true},
   adapter: netlify({
     edgeMiddleware: true
